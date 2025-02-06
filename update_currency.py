5b6c6668e611aa6e8fbc9f96b6c77bf3b65f6a67
leojjw <leojjw@naver.com>
2025-02-06 16:46:24 +0900

    api 변경

--- update_currency.py
+++ update_currency.py
@@ -4,37 +4,32 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
-today = datetime.today().strftime("%Y%m%d")
+today = datetime.today().strftime("%Y-%m-%d")
 API_KEY = os.getenv("AUTHKEY")
-URL = f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={API_KEY}&searchdate={today}&data=AP01"
+URL = f"https://{today}.currency-api.pages.dev/v1/currencies/krw.json"
+
 
 README_PATH = "README.md"
 
 def get_currency():
     response = requests.get(URL)
     if response.status_code == 200:
-        data = response.json()
-        currency = []
-        for cur in data:
-            cur_nm = cur["cur_nm"]
-            ttb = cur["ttb"]
-            tts = cur["tts"]
-            currency.append(f"| {cur_nm} | {ttb} | {tts} |")
-        return currency
+        data = response.json()['krw']
+        return list(map(lambda x: f"| {x[0]} | {round(1/x[1], 2)} |", data.items()))
     else:
         return []
 
 def update_readme():
     """README.md 파일을 업데이트 (표 형식 적용)"""
     currency_info = get_currency()
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     if not currency_info:
         currency_table = "환율 정보를 가져오는 데 실패했습니다."
     else:
         # 마크다운 표 헤더와 구분선 추가
-        currency_table = """| 국가/통화명 | 전신환(송금)받으실때 | 전신환(송금)보내실때 |
-|------------|------------------|------------------|
+        currency_table = """| 통화코드 | 환율 |
+|------------|------------------|
 """
         currency_table += "\n".join(currency_info)
 
@@ -45,7 +40,7 @@ def update_readme():
 
 {currency_table}
 
-⏳ 업데이트 시간: {now} (UTC)
+⏳ 업데이트 날짜: {today}
 
 ---
 자동 업데이트 봇에 의해 관리됩니다.
